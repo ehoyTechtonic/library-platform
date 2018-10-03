26b14d1433299f211d68391fb0428028e539f3c6
Erik Hoy <>
2018-10-03 15:03:43 -0600

    fixed getBookByAuthor and getBookByTitle to only partially match title or author

--- js/book.js
+++ js/book.js
@@ -6,5 +6,16 @@ function Book (title, author, numPages, pubDate) {
 }
 
 Book.prototype.editBook = function(oBook) {
-
+  for (i=0; i<this.bookShelf.length; i++) {
+    if (this.bookShelf[i] === oBook.title) {
+      this.bookShelf[i].title = oBook.title;
+      this.bookShelf[i].author = oBook.author;
+      this.bookShelf[i].numPages = oBook.numPages;
+      this.bookShelf[i].pubDate = oBook.pubDate;
+    }
+  }
 }
+// 
+// document.addEventListener("DOMContentLoaded", function(e) {
+//   window.gBook = new Book();
+// });
--- js/engine.js
+++ js/engine.js
@@ -5,7 +5,6 @@ function Library() {
 Library.prototype.addBook = function(book) {
   for (i=0; i<this.bookShelf.length; i++) {
     if (this.bookShelf[i].title === book.title) {
-      // gLibrary.saveBook(book);
       return false;
     }
   }
@@ -29,20 +28,24 @@ Library.prototype.removeBookByTitle = function(title) {
 //Array.length is dynamic?
 Library.prototype.removeBookByAuthor = function(author) {
   if(this.bookShelf.length > 0) {
-    var x = this.bookShelf.length;
+    // var x = this.bookShelf.length;
     // for (i=0; i<x; i++) {
+    var removed = "";
     for (var i in this.bookShelf) {
-      // console.log(this.bookShelf[i]);
       if (this.bookShelf[i].author === author) {
-        this.bookShelf.splice(i,1);
+        removed += this.bookShelf.splice(i,1);
       }
+    }
+    if (removed.length > 0) {
+      return true;
+    } else {
+      return false;
     }
   }
 };
 
 Library.prototype.getRandomBook = function() {
   randomNumber = Math.floor(Math.random() * this.bookShelf.length);
-  console.log(this.bookShelf[randomNumber]);
   if (this.bookShelf[randomNumber].length > 0) {
     return this.bookShelf[randomNumber];
   } else {
@@ -51,30 +54,30 @@ Library.prototype.getRandomBook = function() {
 };
 
 Library.prototype.getBookByTitle = function(title) {
-  for (var i in this.bookShelf) {
-    if (this.bookShelf[i].title === title) {
-      console.log(this.bookShelf[i]);
-    }
+  function getBookArray(book) {
+    return book.title.includes(title);
   }
+  var bookArray = this.bookShelf.filter(getBookArray);
+  return bookArray;
 };
 
 Library.prototype.getBookByAuthor = function(author) {
-  var authorArray = [];
-  for (var i in this.bookShelf) {
-    if (this.bookShelf[i].author === author) {
-      authorArray.push(this.bookShelf[i])
-    }
+  function getAuthorArray(book) {
+    return book.author.includes(author);
   }
-  console.log(authorArray);
+  var authorArray = this.bookShelf.filter(getAuthorArray);
+  return authorArray;
 };
 
 Library.prototype.addBooks = function() {
-  console.log(arguments.length);
   for (j=0; j<arguments.length; j++) {
-    console.log(arguments[j]);
     this.addBook(arguments[j]);
   }
-  return true;
+  if (arguments.length > 0) {
+    return arguments.length;
+  } else {
+    return 0;
+  }
 };
 
 Library.prototype.getAuthors = function() {
@@ -84,13 +87,16 @@ Library.prototype.getAuthors = function() {
       authorArray.push(this.bookShelf[i].author);
     }
   }
-  console.log(authorArray);
-  return true;
+  return authorArray;
 };
 
 Library.prototype.getRandomAuthorName = function() {
   randomNumber = Math.floor(Math.random() * this.bookShelf.length);
-  console.log(this.bookShelf[randomNumber].author);
+  if (this.bookShelf.length > 0) {
+    return this.bookShelf[randomNumber].author;
+  } else {
+    return null;
+  }
 };
 
 document.addEventListener("DOMContentLoaded", function(e) {
